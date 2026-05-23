8417ab68a5dd590ebbea60bc6853f2304c5ce32c
Juan Lassalle <135929039+juanlassalle@users.noreply.github.com>
2026-05-23 16:58:51 -0300

    Se agrega ejercicio_2 simil ejercicio_1

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # FuncionesCsharp
 1-Se requiere un algoritmo para obtener la suma de diez cantidades mediante
-la utilización de un ciclo "Mientras". Usar Métodos para resolver el ejercicio
+la utilización de un ciclo "Mientras". Usar Métodos para resolver el ejercicio.
+2-Se requiere un algoritmo para obtener la suma de diez cantidades mediante
+la utilización de un ciclo Repite. Usando dos métodos para resolver el ejercicio.
+El primer método se encargará de solicitar los datos y el segundo método se
+encargará de realizar la suma
---
+++ Solution1/Ejercicio_2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_2
+{
+    internal class Program
+    {
+        //Se requiere un algoritmo para obtener la suma de diez cantidades mediante
+        //la utilización de un ciclo Repite. Usando dos métodos para resolver el ejercicio.
+        //El primer método se encargará de solicitar los datos y el segundo método se
+        //encargará de realizar la suma.
+
+        private static int suma = 0;
+        static void Main(string[] args)
+        {
+            SolicitarDatos();
+            Console.WriteLine($"Los valores sumados son: {suma}");
+        }
+        private static void SolicitarDatos()
+        {
+            int valor;
+            int contador = 1;
+
+            do
+            {
+                Console.Write("Ingresar valor: ");
+                valor = Convert.ToInt32(Console.ReadLine());
+
+                SumarValores(valor);
+                contador++;
+            }
+            while (contador <= 10);
+        }
+
+        private static void SumarValores(int _valor)
+        {
+            suma = suma + _valor;
+            
+        }
+
+    }
+}
