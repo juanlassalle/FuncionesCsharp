e05e8a3d4d58189992ad58a7e4c98e70756ebb0c
Juan Lassalle <135929039+juanlassalle@users.noreply.github.com>
2026-05-22 23:49:16 -0300

    se agrega el ejercicio_1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # FuncionesCsharp
+1-Se requiere un algoritmo para obtener la suma de diez cantidades mediante
+la utilización de un ciclo "Mientras". Usar Métodos para resolver el ejercicio
---
+++ Solution1/Ejercicio_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    internal class Program
+    {
+        //Se requiere un algoritmo para obtener la suma de diez cantidades mediante
+        //la utilización de un ciclo "Mientras". Usar Métodos para resolver el ejercicio.
+        static void Main(string[] args)
+        {
+            SumarDiezCantidades();
+        }
+
+        private static void SumarDiezCantidades()
+        {
+            int suma = 0;
+            int contador = 1;
+            int valor;
+
+            while (contador <= 10)
+            {
+                Console.Write("Ingrese el valor: ");
+                valor = Convert.ToInt32(Console.ReadLine());
+                suma = suma + valor;
+                contador++;
+            }
+            Console.WriteLine($"La suma de los valores es {suma}");
+        }
+    }
+}
