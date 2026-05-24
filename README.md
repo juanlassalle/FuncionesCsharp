1d91a544ccfd7ec51929dfeb1348978e53577b33
Juan Lassalle <135929039+juanlassalle@users.noreply.github.com>
2026-05-23 22:12:44 -0300

    se agrega ejercicio_3 simil a los dos primeros

--- README.md
+++ README.md
@@ -4,4 +4,8 @@ la utilización de un ciclo "Mientras". Usar Métodos para resolver el ejercicio
 2-Se requiere un algoritmo para obtener la suma de diez cantidades mediante
 la utilización de un ciclo Repite. Usando dos métodos para resolver el ejercicio.
 El primer método se encargará de solicitar los datos y el segundo método se
-encargará de realizar la suma
+encargará de realizar la suma.
+3-Se requiere un algoritmo para obtener la suma de diez cantidades mediante
+la utilización de un ciclo FOR. Usando dos métodos para resolver el ejercicio.
+El primer método se encargará de solicitar los datos y el segundo método se
+encargará de realizar la suma y el tercero de imprimir la suma.
---
+++ Solution1/Ejercicio_3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_3
+{
+    //Se requiere un algoritmo para obtener la suma de diez cantidades mediante
+    //la utilización de un ciclo FOR. Usando dos métodos para resolver el ejercicio.
+    //El primer método se encargará de solicitar los datos y el segundo método se
+    //encargará de realizar la suma.
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ImprimirSuma();
+        }
+
+        private static void ImprimirSuma()
+        {
+            Console.WriteLine($"La suma de los valores es {SolicitarDatos()}");
+        }
+
+        private static int SolicitarDatos()
+        {
+            int suma = 0;
+            int valor;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"Ingresar valor {i}: ");
+                valor = Convert.ToInt32(Console.ReadLine());
+
+                suma = SumarValores(suma, valor);
+            }
+
+            return suma;
+        }
+        private static int SumarValores(int _suma, int _valor)
+        {
+            return _suma + _valor;
+        }
+    }
+}
